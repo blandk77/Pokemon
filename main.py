7e6601ece46c9e83a9455bd7f38ad0a7d8205a51
blandk77 <alexags1983@gmail.com>
2025-03-29 19:52:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from pyrogram.types import BotCommand
 from config import API_ID, API_HASH, BOT_TOKEN
 from Bot.commands import start, help, settings
 from Bot.handlers import message_handler
-from flask import Flask, render_template, request
 from aiohttp import web
+from aiohttp_wsgi import WSGIHandler  # Import WSGIHandler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -19,10 +19,13 @@ logging.basicConfig(
 bot = Client("EncodingBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 async def run_flask_app():
-    from Bot.commands import settings  # Import settings here to avoid circular imports
-    app = settings.app  # Access the Flask app from settings.py
+    # from Bot.commands import settings  # Import settings here to avoid circular imports
+    # app = settings.app  # Access the Flask app from settings.py
 
-    runner = web.AppRunner(app)
+    wsgi_handler = WSGIHandler(settings.app)  # Create a WSGIHandler for Flask app
+    aiohttp_app = web.Application()  # Create an aiohttp application
+    aiohttp_app.router.add_route("*", "/{tail:.*}", wsgi_handler) # All routes to Flask
+    runner = web.AppRunner(aiohttp_app) # Run aiohttp
     await runner.setup()
     site = web.TCPSite(runner, '0.0.0.0', 5000)  # Use config.FLASK_HOST/PORT from config.py
     await site.start()
@@ -52,9 +55,10 @@ async def main():
 
     print(f"@{bot_username} started successfully!")
 
+    # Start Flask app in a separate task
     flask_task = asyncio.create_task(run_flask_app())
 
-    await asyncio.gather(asyncio.sleep(1), flask_task, bot.idle())
+    await asyncio.gather(asyncio.sleep(1), flask_task, bot.idle())  # Run both concurrently, bot.idle to wait for bot termination
 
     await bot.stop()
 
